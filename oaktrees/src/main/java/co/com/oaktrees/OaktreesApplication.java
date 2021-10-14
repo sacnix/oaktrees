047c8b140971c1c0e75bb2430a25d1ac254da38b
Dayana Murillo <dahia.m685@gmail.com>
2021-10-13 20:24:39 -0500

    Main project

---
+++ oaktrees/src/main/java/co/com/oaktrees/OaktreesApplication.java
@@ -0,0 +1,13 @@
+package co.com.oaktrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaktreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaktreesApplication.class, args);
+	}
+
+}
